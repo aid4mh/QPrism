629cf32c2d3d7a3bb2c8c070fa26ab137fd8af54
Jana Kabrit <jana.kabrit@outlook.com>
2022-06-29 14:08:39 +0300

    added logarithmic option

--- modules/Audio/spectrogram.py
+++ modules/Audio/spectrogram.py
@@ -4,15 +4,16 @@ from scipy.io import wavfile
 import numpy as np
 
 
-def aud_spectrogram(wav_path):
+def aud_spectrogram(wav_path, log=False):
     """
     A spectrogram is a figure which represents the spectrum of frequencies of a recorded audio over time. 
 
-    This function returns the spectrogram of the audio.
+    This function returns the spectrogram of the audio with the option of a logarithmic scale.
 
     Parameters
     -----------
     wav_path: path to a .wav audio
+    log (Optional, Default=False): If true, displays the spectrogram with a logarithmic scale.
 
     Returns
     -------
@@ -23,5 +24,7 @@ def aud_spectrogram(wav_path):
     frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
     plt.pcolormesh(np.log(spectrogram))
     plt.ylabel('Frequency [Hz]')
+    if log == True:
+        plt.yscale('symlog')
     plt.xlabel('Time [sec]')
     plt.show()
