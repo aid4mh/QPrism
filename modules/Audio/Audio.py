c96eb0654b958a8f96819177f7e470675848bbb3
Jana Kabrit <jana.kabrit@outlook.com>
2022-07-20 22:03:24 +0300

    ignore hidden files

--- modules/Audio/Audio.py
+++ modules/Audio/Audio.py
@@ -5,13 +5,13 @@ import pandas as pd
 from pathlib import Path
 
 # Audio functions
-# from modules.Audio.audio_classification import audio_classification
+from modules.Audio.audio_classification import audio_classification
 from modules.Audio.audio_length import audio_length
 from modules.Audio.audio_sample_rate import sample_rate
 from modules.Audio.audio_rms import rms
 from modules.Audio.snr import snr
 
-# Helpers 
+# Helpers
 from modules.Audio.helpers.audio_to_array import to_array
 from modules.Audio.helpers.audio_paths import audio_paths
 from modules.Audio.helpers.to_mono_wav import to_mono_wav
@@ -19,14 +19,13 @@ from modules.Audio.helpers.to_mono_wav import to_mono_wav
 
 class Audio:
     def __init__(self):
-        self.aud_metrics = ['classification', 'audio_length', 'sample_rate', 'rms'\
+        self.aud_metrics = ['classification', 'audio_length', 'sample_rate', 'rms'
                             'signal_to_noise']
 
-
-    def length(self, path:str):
+    def length(self, path: str):
         """
         Get the length of the audio
-        
+
         This functions calls the audio_length function and 
         gives the length for both a folder of audios or a single audio file.
 
@@ -45,20 +44,20 @@ class Audio:
         if os.path.isdir(path):
             audio_files = audio_paths(path)
             length = {}
-            
+
             for file in audio_files:
-                length[str(Path(file).stem)] = audio_length(file)
-            
+                if not file.startswith('.'):
+                    length[str(Path(file).stem)] = audio_length(file)
+
             return pd.DataFrame(length.items(), columns=['Audios', 'Length'])
 
         else:
             return audio_length(path)
 
-    
-    def sample_rate(self, path:str):
+    def sample_rate(self, path: str):
         """
         Get the sample rate of the audio
-        
+
         This functions calls the sample_rate function and 
         gives the bit rate for both a folder of audios or a single audio file.
 
@@ -79,15 +78,15 @@ class Audio:
             s_rate = {}
 
             for file in audio_files:
-                s_rate[str(Path(file).stem)] = sample_rate(file)
-            
+                if not file.startswith('.'):
+                    s_rate[str(Path(file).stem)] = sample_rate(file)
+
             return pd.DataFrame(s_rate.items(), columns=['Audios', 'Sample Rate'])
 
         else:
             return sample_rate(path)
 
-    
-    def audio_classify(self, path:str):
+    def audio_classify(self, path: str):
         """
         Get a list of all the sounds in an audio
 
@@ -109,15 +108,15 @@ class Audio:
             sounds = {}
 
             for file in audio_files:
-                sounds[str(Path(file).stem)] = audio_classification(file)
-            
+                if not file.startswith('.'):
+                    sounds[str(Path(file).stem)] = audio_classification(file)
+
             return pd.DataFrame(sounds.items(), columns=['Audios', 'Voices'])
-        
+
         else:
             return audio_classification(path)
 
-    
-    def root_mean_square(self, path:str):
+    def root_mean_square(self, path: str):
         """
         RMS level (root mean squared) is just proportional to the amount of energy over a period of time in the signal. This can be used to distinguish audios that are louder from each other.
         This function returns the rms value of a given function.
@@ -137,15 +136,15 @@ class Audio:
             rms_values = {}
 
             for file in audio_files:
-                rms_values[str(Path(file).stem)] = rms(file)
-            
+                if not file.startswith('.'):
+                    rms_values[str(Path(file).stem)] = rms(file)
+
             return pd.DataFrame(rms_values.items(), columns=['Audios', 'RMS'])
 
         else:
             return rms(path)
 
-
-    def signaltonoise(self, path:str):
+    def signaltonoise(self, path: str):
         """
         RMS level (root mean squared) is just proportional to the amount of energy over a period of time in the signal. This can be used to distinguish audios that are louder from each other.
         This function returns the rms value of a given function.
@@ -165,14 +164,15 @@ class Audio:
             snr_values = {}
 
             for file in audio_files:
-                snr_values[str(Path(file).stem)] = round(snr(file), 3)
-            
+                if not file.startswith('.'):
+                    snr_values[str(Path(file).stem)] = round(snr(file), 3)
+
             return pd.DataFrame(snr_values.items(), columns=['Audios', 'SNR'])
 
         else:
             return round(snr(path), 3)
 
-    def all_audio(self, path:str):
+    def all_audio(self, path: str):
         """
         Creates a csv file with all the audio metrics
 
@@ -191,11 +191,12 @@ class Audio:
             audio_files = [os.path.abspath(path)]
 
         with open('audio_metrics.csv', 'w') as file:
-            header = ['Audio Name', 'Length', 'Sample Rate', 'Voices', 'SNR', 'RMS']
+            header = ['Audio Name', 'Length',
+                      'Sample Rate', 'Voices', 'SNR', 'RMS']
             writer = csv.writer(file)
             writer.writerow(header)
 
-        csv_path = str(os.getcwd()) + '/audio_metrics.csv'  
+        csv_path = str(os.getcwd()) + '/audio_metrics.csv'
         metrics = pd.read_csv(csv_path)
 
         for i, audio in enumerate(audio_files):
@@ -210,6 +211,6 @@ class Audio:
 
             data_series = pd.Series(data_row, index=metrics.columns)
             metrics = metrics.append(data_series, ignore_index=True)
-        
+
         print('Metrics saved to ', csv_path)
         metrics.to_csv(csv_path, index=False)
