94ac7d2207f762a8271ce4be4ada2747a0d41afd
linzixio <jueji.lin@mail.utoronto.ca>
2022-07-20 00:20:39 -0400

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../modules'))
-import module.Sensor.load_data as load_data
+import Sensor.load_data as load_data
 # -- Project information
 
 project = 'QPrism'
