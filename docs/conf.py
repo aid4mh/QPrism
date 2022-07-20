b2ccf566d6d1616b179138be12185479924728c2
linzixio <jueji.lin@mail.utoronto.ca>
2022-07-19 23:56:13 -0400

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information
 
@@ -29,7 +32,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
