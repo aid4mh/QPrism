1fbb714517887fcaae6789d4fe3743cd9dc8c838
Lin Zixiong <linzixio@teach.cs.toronto.edu>
2022-09-14 15:01:44 -0400

    Adding data processing helper function

--- QPrism/Sensor/data_processing.py
+++ QPrism/Sensor/data_processing.py
@@ -13,3 +13,16 @@ def scale_data(data):
     scaled_data = pd.DataFrame(scaled_data)
     scaled_data.columns = data.columns
     return scaled_data
+
+
+def timestamp_to_unix(df):
+    """
+        Convert the timestamp in the dataframe to unix format.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            A dataframe represents a single record, with the timestamp in the pandas datetime format.
+    """
+    timestamp_key = df.keys()[0]
+    df[timestamp_key] = df[timestamp_key].astype('int64')
